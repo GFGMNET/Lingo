47f07914c5181f037081aa80e0ebaddbbc4d2c67
Your Name <you@example.com>
2023-07-14 07:49:21 +0200

    Automatic Transfer

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const fs = require('fs'
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 
 // body parse middleware application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,6 +17,26 @@ app.use(bodyParser.json());
 
 //set public folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get("/", (req, res) => {
+    const blogposts = fs.readdirSync(__dirname + '/content/blog').filter(file => file.endsWith('.md'));
+    const posts = blogposts.map((fileName) => {
+      const slug = fileName.replace('.md', '');
+      const readFile = fs.readFileSync(`content/blog/${fileName}`, 'utf-8');
+      const { data: frontmatter } = matter(readFile);
+      return {
+        slug,
+        frontmatter,
+      };
+    })
+    console.log(posts);
+     res.render("index", {
+       title: "Home" ,
+       posts: posts
+     });
+  
+  });
 
 
 app.listen('3000', () => {
